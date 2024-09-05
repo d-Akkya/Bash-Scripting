92a793be0ac42b1dcb432b158a110d794d379423
d-Akkya <akhileshbamane26@gmail.com>
2024-09-06 00:02:33 +0530

    Operators

---
+++ 09_operators.sh
@@ -0,0 +1,13 @@
+#Operators in bash
+
+1) -eq/==   #Equal
+
+2) -gt 	    #GreaterThan
+
+3) -lt      #LessThan
+
+4) -ge      #GreaterThanOrEqualTo
+
+5) -le      #LessThanOrEqualTo
+
+6) -ne/!=   #NotEqual
