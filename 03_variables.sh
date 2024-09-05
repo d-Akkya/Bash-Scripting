b648a3814f3dbb01241e8b783e4bac4d8df45ad3
d-Akkya <akhileshbamane26@gmail.com>
2024-09-05 15:09:43 +0530

    Discussion about varialbles (store output of commands, constant variable)

--- 03_variables.sh
+++ 03_variables.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #Script to show how to use variables
 
 a=10
@@ -6,7 +7,18 @@ age=22
 
 echo "My name is $name and age is $age."
 
-name='Akhilesh'
+name='Akhilesh' #We can change the values of variable
 
 echo "My name is $name"
+
+#Variable to store the output of a command
+HOSTNAME=$(hostname)   #hostname is command in Linux which shows the name of machine
+echo "Name of this machine is $HOSTNAME"
+
+#Constant Variable (readonly is the keyword for constant)
+readonly College="PVPIT"
+echo "My College name is $College"
+
+College="AITRC"     #shows error
+echo $College
 
