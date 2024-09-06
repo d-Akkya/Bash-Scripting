9e3cdb4c345492248b7ce55c47aa9f5f8435f345
d-Akkya <akhileshbamane26@gmail.com>
2024-09-06 23:56:34 +0530

    update code to remove row no.1

--- 23_whileLoop_with_csv.sh
+++ 23_whileLoop_with_csv.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-while IFS="," read id name age       #IFS=internal field seperator
+cat test.csv | awk 'NR!=1 {print}' | while IFS="," read id name age       #IFS=internal field seperator
 do 
 	echo "Id is $id"
 	echo "Name is $name"
 	echo "Age is $age"
-done < test.csv
+done
