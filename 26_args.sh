3cb08cbd3c227fe88680f8874e9752a1219ed244
d-Akkya <akhileshbamane26@gmail.com>
2024-09-08 18:49:04 +0530

    access & for-loop on args while scripting, exit keyword

--- 26_args.sh
+++ 26_args.sh
@@ -10,6 +10,12 @@ comment
 
 #to access the argument
 
+if [[ $# -eq 0 ]]
+then 
+	echo "Not provided any argument"
+	exit 1        #this keyword stops the script at a point
+fi
+
 echo "First argument is $1"
 echo "Second argument is $2"
 
