fa47050390bc0460cdde3f5a5b34be40b10e8c20
d-Akkya <akhileshbamane26@gmail.com>
2024-09-08 16:09:14 +0530

    access & for-loop on args while scripting

--- 26_args.sh
+++ 26_args.sh
@@ -15,3 +15,10 @@ echo "Second argument is $2"
 
 echo "All the arguments are - $@"
 echo  "Number of arguments are - $#"
+
+#for-loop to access the values from arguments
+
+for filename in $@
+do
+	echo "Copying file - $filename"
+done
