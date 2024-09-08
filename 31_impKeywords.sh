5710a569eb4aa685bf6479f376c54b9419f702a6
d-Akkya <akhileshbamane26@gmail.com>
2024-09-08 19:21:54 +0530

    basename, dirname & realpath

---
+++ 31_impKeywords.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#basename - only last file name
+basename f:/myScripts/names.txt
+
+#dirname - dir name in which file exists
+dirname f:/myScripts/test.csv
+
+#realpath - provides full path of file
+realpath f:/myScripts/names.txt
