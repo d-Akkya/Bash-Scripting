eb89c981419d9b31958ec333fbde46a9c2158844
d-Akkya <akhileshbamane26@gmail.com>
2024-09-08 15:33:28 +0530

    task: calculator project

--- calculator.sh
+++ calculator.sh
@@ -2,11 +2,11 @@
 
 #Simple task of Calculator project
 
-#create functions
+#creating functions
 
 function add {
-	read -p "Enter the first number: " num1
-	read -p "Enter the second number: " num2
+	read -p "Enter first number: " num1
+	read -p "Enter second number: " num2
 	let sum=$num1+$num2
 	echo "Addition of $num1 + $num2 is $sum"
 }
@@ -14,24 +14,23 @@ function add {
 function sub {
 	read -p "Enter first number: " num1
 	read -p "Enter second number: " num2
-	echo "Substraction of $num1-$num2 is $(($num-$num2))"
+	let result=$num1-$num2
+	echo "Substraction of $num1-$num2 is $result"
 }
 
 function mul {
-	read -p "Enter the first number: " num1
-	read -p "Enter the second number: " num2
+	read -p "Enter first number: " num1
+	read -p "Enter second number: " num2
 	echo "Multiplication of $num1*$num2 is $(($num1*$num2))"
 }
 
 function div {
-	echo "Enter the first number: $num1"
-	echo "Enter the second number: $num2"
-	let div=$num1/$num2
-	echo "Division of $num1/$num2 is $div "
+	read -p "Enter first number: " num1
+	read -p "Enter second number: " num2
+	echo "Division of $num1/$num2 is $(( $num1/$num2 ))"
 }
 
-#create case
-
+#creating case statement
 echo "Select the operation you want perform of Calculator"
 echo "a: Addition"
 echo "b: Substraction"
