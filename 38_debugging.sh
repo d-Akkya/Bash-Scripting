50e6cf9f8538a8e718aca1b25e8146e00327cd7a
d-Akkya <akhileshbamane26@gmail.com>
2024-09-09 13:03:08 +0530

    debugging & stop the script

---
+++ 38_debugging.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -x     #this keyword used to test and debug the script, as it step by step displays the flow of script
+
+set -e     #this keyword stops the script whenever command fails
+
+pwd
+whoami
+date
+cd /root
+hostname
